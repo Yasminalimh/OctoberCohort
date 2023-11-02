5c79ff9f166639d5a2c9c3ba76e6d541eb84f1f1
Yasmin <yasminalimhh@gmail.com>
2023-11-02 20:36:01 +0100

    Lesson 1 Javascript

---
+++ Javascript/recording1.js
@@ -0,0 +1,40 @@
+//this is a comment 
+
+//camelCase = when wriritng sentces the second word Captial
+//Ex: camelCaseSecondCaseThirdCase
+
+//understanding different types of variables 
+
+//const cant be reassgined
+const myName = "Yasmin"
+console.log(myName)
+
+//Var stands for global variable 
+var Today = "Tuesday"
+console.log(Today)
+
+//let can be reassigned 
+let Tomorrow = "Wednesday"
+console.log(Tomorrow)
+
+Tomorrow = "Thursday"
+console.log(Tomorrow)
+
+
+//data-types
+
+// "" or '' or ’’  - string data type
+
+// 0-9 - number data type
+
+// true/false - bolean data/type
+
+//undefined data - type
+
+//how to check what data-type it is:
+
+console.log(typeof myName)
+
+
+
+
