64967475a0df1df3b70a97b1d8999de5a9c72cb0
Yasmin <yasminalimhh@gmail.com>
2023-12-02 15:31:34 +0100

    Loops part 1

---
+++ Javascript/recording6.js
@@ -0,0 +1,21 @@
+//for loops
+
+/*
+for(let i=0; i<=10; i++){
+    console.log("Hello " + i)
+}
+*/
+
+/*
+for(let i=0; i<=10; i+=2){
+    console.log("Hello " + i)
+}
+*/
+
+for(let i=10; i>=0; i--){
+    if(i===0){
+        console.log("Blast OFF!!!")
+    }else{
+        console.log(i)
+    }
+}
